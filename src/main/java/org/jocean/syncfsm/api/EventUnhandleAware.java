0f5233f1d3c454371949b35059ae63ec1149ad92
ming.ma <ming.ma@upai.com>
2014-04-03 12:03:05 +0800

    将 EventUnhandleAware 从 Eventable 派生

--- src/main/java/org/jocean/syncfsm/api/EventUnhandleAware.java
+++ src/main/java/org/jocean/syncfsm/api/EventUnhandleAware.java
@@ -7,6 +7,6 @@ package org.jocean.syncfsm.api;
  * @author isdom
  *
  */
-public interface EventUnhandleAware {
+public interface EventUnhandleAware extends Eventable {
     public void onEventUnhandle(final String event, final Object ... args) throws Exception;
 }
